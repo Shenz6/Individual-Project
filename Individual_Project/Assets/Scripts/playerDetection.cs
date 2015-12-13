12236af89b565d9b648062e735f0ccc7c06b2194
William <williamsamuels@blueyonder.co.uk>
2015-12-13 21:54:11 +0000

    Testing plane added and 3 scrips for enemy

--- Individual_Project/Assets/Scripts/playerDetection.cs
+++ Individual_Project/Assets/Scripts/playerDetection.cs
@@ -2,7 +2,37 @@
 using System.Collections;
 
 public class playerDetection : MonoBehaviour {
+/*
+	public float fieldOfViewAngle = 110f;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
 
+	private NavMeshAgent nav;
+	private SphereCollider col;
+	private Animator anim;
+	private LastPlayerSighting lastPlayerSighting;
+	private GameObject player;
+	private Animator playerAnim;
+	private PlayerHealth playerHealth;
+	private HashIDs hash;
+	private Vector3 previousSighting;
+
+	void Awake()
+	{
+		nav = GetComponent<NavMeshAgent> ();
+		col = GetComponent<SphereCollider> ();
+		anim = GetComponent<Animator> ();
+		lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<lastPlayerSighting> ();
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+		playerAnim = player.GetComponent<Animator> ();
+		playerHealth = player.GetComponent<playerHealth> ();
+		hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+
+		personalLastSighting = lastPlayerSighting.resetPosition;
+		previousSighting = lastPlayerSighting.resetPosition;
+
+	}
+*/
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +40,15 @@ public class playerDetection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		RaycastHit hit;
+		float theDistance;
+		// debug raycast 
+		Vector3 forward = transform.TransformDirection (Vector3.forward) *10;
+		Debug.DrawRay(transform.position,forward,Color.green);
+
+		if(Physics.Raycast(transform.position,(forward), out hit)){
+			theDistance = hit.distance;
+			print (theDistance + " " + hit.collider.gameObject.name);
+		}
 	}
 }
---
+++ Individual_Project/Assets/Scripts/playerFollow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerFollow : MonoBehaviour {
+
+	public float fpsTargetDistance;
+	public float enemyLookDistance;
+	public float attackDistance;
+	public float EnemyMovementSpeed;
+	public float damping = 1;
+	public Transform fpsTarget;
+
+	Rigidbody theRigidBody;
+	public Renderer myRender;
+
+	// Use this for initialization
+	void Start () {
+
+
+		myRender.GetComponent<Renderer> ();
+		theRigidBody = GetComponent<Rigidbody>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		fpsTargetDistance = Vector3.Distance (fpsTarget.position,transform.position);
+
+		if (fpsTargetDistance < enemyLookDistance) {
+			myRender.material.color = Color.yellow;
+			lookAtPlayer();
+			print ("looking at player");
+		}
+		if (fpsTargetDistance < attackDistance) {
+			myRender.material.color = Color.red;
+			attackPlease();
+			print ("Attack");
+
+		}
+		if(fpsTargetDistance > enemyLookDistance) {
+			myRender.material.color = Color.green;
+		}
+	}
+
+	void lookAtPlayer(){
+
+		Quaternion rotation = Quaternion.LookRotation (fpsTarget.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * damping);
+
+	}
+
+	void attackPlease(){
+
+		transform.position = Vector3.MoveTowards (transform.position, fpsTarget.transform.position, EnemyMovementSpeed);
+
+		//theRigidBody.AddForce (transform.forward * EnemyMovementSpeed);
+	
+	}
+}

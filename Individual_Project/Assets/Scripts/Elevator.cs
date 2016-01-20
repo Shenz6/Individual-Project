1ae218f8d519870aaf8a9d71c586a26d1841b489
William <williamsamuels@blueyonder.co.uk>
2016-01-20 17:53:24 +0000

    Key and elevator
    Key model added and base for elevator. key script not letting player
    pick it up

---
+++ Individual_Project/Assets/Scripts/Elevator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	private bool doorOpen = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doorOpen == true) {
+			transform.Translate(new Vector3(0,2,0));
+		}
+	
+	}
+	//this is part of what to change for more keys
+	void onTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "player" && GameVariables.keyCount > 0) {
+			GameVariables.keyCount-=1;
+			doorOpen = true;
+			//keyCollected += 1;
+			//GameVariables.keyCount+=1;
+			//Destroy(gameObject);
+			
+		}
+	}
+}
---
+++ Individual_Project/Assets/Scripts/Key.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+	
+	void onTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Player") {
+			//keyCollected += 1;
+			Debug.Log ("Collision with key");
+			GameVariables.keyCount+=1;
+			Destroy(gameObject);
+
+		}
+	}
+}

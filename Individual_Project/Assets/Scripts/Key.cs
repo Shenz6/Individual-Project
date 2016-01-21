e2a1f6f8afcf7a48be347ad362c505d03ef5b28f
William1995 <k1307574@kingston.ac.uk>
2016-01-21 14:17:39 +0000

    Fixed Key Pick Up
    when using on trigger make sure which letters need to be capitalised
    
    OnTriggerEnter()

--- Individual_Project/Assets/Scripts/Key.cs
+++ Individual_Project/Assets/Scripts/Key.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class Key : MonoBehaviour {
 	
-	void onTriggerEnter(Collider collider)
+	void OnTriggerEnter(Collider collider)
 	{
+        Debug.Log("trigger entered");
 		if (collider.gameObject.tag == "Player") {
 			//keyCollected += 1;
 			Debug.Log ("Collision with key");
 			GameVariables.keyCount+=1;
 			Destroy(gameObject);
+
+            Debug.Log(GameVariables.keyCount);
 
 		}
 	}
